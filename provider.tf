52f845472888043fa95d255c43d76e14e1b58b40
Uday <udayp081198@gmail.com>
2024-06-21 19:17:29 +0530

    test

--- provider.tf
+++ provider.tf
@@ -6,7 +6,7 @@ terraform {
   backend "s3" {
     bucket  = "test-atlantis-easydeploy"
     key     = "atlantis/terraform.tfstate"
-    region  = "us-east-1"
+    region  = "us-east-1" 
     profile = "default"
   }
 }
