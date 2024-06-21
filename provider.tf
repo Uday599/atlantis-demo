16e341d6f6f057ac2d1ae1c2f678a440266bc60c
Uday <udayp081198@gmail.com>
2024-06-21 19:20:19 +0530

    Ec2 created

--- provider.tf
+++ provider.tf
@@ -10,3 +10,13 @@ terraform {
     profile = "default"
   }
 }
+
+resource "aws_instance" "web" {
+  # ami           = data.aws_ami.ubuntu.id
+  ami = "ami-0e001c9271cf7f3b9"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "terraform-atlantis"
+  }
+}
